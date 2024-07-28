4aafb44bcce2ffef907f24a4d58f8562fccc6579
Sagar Deriv <sagar@deriv.com>
2024-07-28 19:23:30 +0400

    docs: add code docs for the interface

--- lib/features/beneficiary/service/beneficiary_service.dart
+++ lib/features/beneficiary/service/beneficiary_service.dart
@@ -1,10 +1,13 @@
 import 'package:top_up_app/features/beneficiary/domain/beneficiary.dart';
 
+/// Service that acts as Interface to application for handling beneficiary related operations.
 abstract class BeneficiaryService {
+  /// Adds a new beneficiary to the user's list of beneficiaries.
   Future<Beneficiary> addNewBeneficiary(
     String userId,
     Beneficiary newBeneficiary,
   );
 
+  /// Fetches the list of beneficiaries for the user.
   Future<List<Beneficiary>> fetchBeneficiaries(String userId);
 }
--- lib/features/top_up/service/topup_service.dart
+++ lib/features/top_up/service/topup_service.dart
@@ -1,8 +1,12 @@
 import 'package:top_up_app/features/top_up/entity/topup_info.dart';
 import 'package:top_up_app/features/top_up/entity/topup_success_entity.dart';
 
+/// Service that acts as Interface to application for handling topup related operations.
 abstract class TopupService {
+  /// Fetches the topup information for the user's beneficiary.
   Future<TopupInfo> fetchTopupInfo(String userId, String beneficiaryId);
+
+  /// Initiates the topup process.
   Future<TopupSuccessEntity> topup(
     String userId,
     String beneficiaryId,
